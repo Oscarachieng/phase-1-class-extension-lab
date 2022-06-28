31d3fc739203dc86bb6a7c88ac70538d2cf0b46a
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-06-28 17:26:09 +0300

    commit tree ff3a2aec0441f77e05c84acf88e220ded88bc9d6

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+// Your code 
+/*
+1.declare classes...
+2. Add the constructor function, takes an array of integers.
+*/
+const arrayOfSides = [5,5,5,5,5]
+ class Polygon {
+    
+     constructor(arrayOfsides){
+       this.sides = arrayOfsides;
+     }
+     get countSides(){
+         return arrayOfsides.length;
+     }
+     get perimeter (){
+         return arrayOfSides.reduce();
+     }
+ }
